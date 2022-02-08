2aec647161bc398ad7253a3bf201ec09367c46b3
wilsonbirch <wilsonbirch@gmail.com>
2022-02-08 15:54:55 -0500

    Completed articles main page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :articles
   root 'pages#home'
   get 'about', to: 'pages#about'
+  resources :articles, only: [:show, :index]
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2022_02_08_183943) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
