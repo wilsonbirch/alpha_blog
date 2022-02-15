5f5966668f8b003daa5b60556961a90df97f4d7f
wilsonbirch <wilsonbirch@gmail.com>
2022-02-15 14:37:09 -0500

    conditionally render buttons depending on logged in user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,12 +10,4 @@ module ApplicationHelper
     image_tag(gravatar_url, alt: user.username, class: "rounded mt-3 shadow mx-auto d-block")
   end
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    !!current_user
-  end
-
 end
