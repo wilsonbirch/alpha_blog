9997f77125a8a8d5a81e3357f7656c03c29ee263
wilsonbirch <wilsonbirch@gmail.com>
2022-02-16 14:21:32 -0500

    7.190

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,29 @@
 class CategoriesController < ApplicationController
 
     def new
+      @category = Category.new
     end
 
     def index
     end
 
     def show
+      @category = Category.find(params[:id])
+    end
+
+    def create
+      @category = Category.new(category_params)
+      if @category.save
+        flash[:notice] = "Category was successfully created"
+        redirect_to @category
+      else
+        render 'new'
+      end
+    end
+
+    private
+
+    def category_params
+      params.require(:category).permit(:name)
     end
 end
